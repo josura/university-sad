5bcc45f4d08c9f12809b2ea39451a4e1de5ea3ed
josura <giorgiolocicero@hotmail.com>
2020-01-29 14:46:44 +0100

    ottimization of the merge for repetition in array to sort , TODO ultimate tipe(half parallel) of algorithm for merge to implement

--- prog_GPU/miecoseprogmoderna/progettoGPU/sorting.sh
+++ prog_GPU/miecoseprogmoderna/progettoGPU/sorting.sh
@@ -1,3 +1,4 @@
+make
 let values=$((1024*1024))
-for i in _hybrid _hybrid_counting _hybrid_counting_lmem _hybrid_counting_vect _hybrid_counting_vectlmem _hybrid_notlmem _hybrid_thread _hybrid_thread_v2 _sliding1; do for j in 128 256 512 ; do echo sorting$i $values $j; OCL_PLATFORM=1 ./sorting$i $values $j | grep sort: ; done; done 
+for i in _hybrid _hybrid_counting _hybrid_counting_lmem _hybrid_counting_vect _hybrid_counting_vectlmem _hybrid_notlmem _hybrid_thread _hybrid_thread_v2 _sliding1 _counting_parallel_merge_no_repetition _counting_parallel_merge_with_repetition  _counting_parallel_merge_with_repetition_V2 _counting_parallel_merge_with_repetition_random _miosort_parallel_merge_no_rep; do for j in 128 256 512 ; do echo sorting$i $values $j; OCL_PLATFORM=1 ./sorting$i $values $j | grep sort: ; done; done 
 OCL_PLATFORM=1 ./mergesorting 512 512
