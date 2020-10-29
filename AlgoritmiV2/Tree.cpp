de0222f30e7466ac5fc768a2188624a761a0e000
josura <giorgiolocicero@hotmail.com>
2020-10-29 01:14:18 +0100

    some errors probably in the Splay procedure because some elements are deleted, whatever

--- AlgoritmiV2/Tree.cpp
+++ AlgoritmiV2/Tree.cpp
@@ -1,17 +1,67 @@
+#include <iostream>
 
 template <class T>
 struct Node//node declaration
 {
-    T key;  
-    Node* left;
-    Node* right;
+    T key;
+    Node<T> *left,*right,*prec;
+    Node() : left(nullptr),right(nullptr),prec(nullptr) {}
+    Node(const T& _key) :key(_key),left(nullptr),right(nullptr),prec(nullptr){}
+    // stack-abusing recursion everywhere, for small code
+    //~Node() { delete left; delete right; delete prec;}
+    int max_depth() const {
+        const int left_depth = left ? left->max_depth() : 0;
+        const int right_depth = right ? right->max_depth() : 0;
+        return (left_depth > right_depth ? left_depth : right_depth) + 1;
+    }
 };
 
 template <class T>
 class Tree{
-    protected Node<T>* root;
+    protected:
+        Node<T>* root;
     public:
-        virtual void Insert(T element) = 0;
-        virtual bool Delete(T element) = 0;
+        Node<T>* getroot(){return root;}
+        T getprec(T el){
+			Node<T>* element=_search(el),*pre;
+			if(element){
+				pre=_prec(element);
+				if(pre)return pre->elem;
+			}
+			return -1;
+		}
+		T getsucc(T el){
+			Node<T>* element=_search(el),*suc;
+			if(element){
+				suc=_succ(element);
+				if(suc)return suc->elem;
+			}
+			return -1;
+		}
+
+        void printBT(const std::string& prefix, const Node<T>* node, bool isLeft)
+        {
+            if( node != nullptr )
+            {
+                std::cout << prefix;
+
+                std::cout << (isLeft ? "├──" : "└──" );
+
+                // print the value of the node
+                std::cout << node->key << std::endl;
+
+                // enter the next tree level - left and right branch
+                printBT( prefix + (isLeft ? "│   " : "    "), node->left, true);
+                printBT( prefix + (isLeft ? "│   " : "    "), node->right, false);
+            }
+        }
+
+        void printBT()
+        {
+            printBT("", root, false);    
+        }
+
+        virtual Tree<T>* Insert(T element) = 0;
+        virtual Tree<T>* Delete(T element) = 0;
         virtual Node<T>* Search(T element) = 0;
 };
--- AlgoritmiV2/example.cpp
+++ AlgoritmiV2/example.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "Splaytree.cpp"
 
-using namespace std;
 
 int main(){
-    Tree<int>* albero = new Splaytree();
+    Tree<int>* albero = new Splaytree<int>();
+    std::cout<<"splaytree"<<std::endl;
+    albero->Insert(1)->Insert(2)->Insert(3)->Insert(4)->Insert(-1)->Insert(20)->Insert(17)->Insert(7);
+    albero->printBT();
+    return 0;
 }
